21fff7b731e20d24953b81a683a76032dff20e3d
za240120522-sys <za240120522@zapopan.tecmm.edu.mx>
2025-08-30 09:04:42 -0600

    version 1: Primeros pasos en Python
    se actualizó la práctica 001 de los primeros pasos en python

---
+++ UNIDAD1/001_HolaMundo.py
@@ -0,0 +1,24 @@
+print("Hola Mundo")
+
+x = 5
+
+print(x)
+#salida: 5
+
+x = 10
+y = 20
+print(x+y)
+
+x = 10 
+if x == 10:
+    print("x es iagual a 10")
+else:
+    print("x no es igual a 10")
+
+
+r= 5.5
+s= 0.5
+print(r, s)
+
+A = "hello guys"
+print(A)
